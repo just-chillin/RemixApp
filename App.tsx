0b801385dcf868f96206d4ec08186258eb3cd1a4
Colin Sivak <abounding05@gmail.com>
2019-10-19 16:03:33 -0400

    Initial Commit

---
+++ App.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <h1>Welcome to Remix!</h1>
+      <TextInput placeholder='email'></TextInput>
+      <TextInput placeholder='password'></TextInput>
+      <Button title='Register'/>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
